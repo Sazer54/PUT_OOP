01efd50e949c75f7feba313900ef8d068fe460ae
Sazer54 <76696930+Sazer54@users.noreply.github.com>
2022-11-17 21:47:24 +0100

    Update ex-1.cpp

--- lesson-4/ex-1.cpp
+++ lesson-4/ex-1.cpp
@@ -1,69 +1,96 @@
+// Udało mi się jakimiś pokrętnymi ścieżkami dojść do tego, jak zwracać metodą obiekt klasy abstrakcyjnej
+// Wystarczy chyba dodać pointer do klasy nadrzędnej, a potem traktować to jak zwykły polimorfizm
+
 #include <iostream>
 #define PREFERABLE_DOGS_EYES_NUMBER 2
 #define PREFERABLE_SPIDERS_EYES_NUMBER 8
+#define MUTATION_TO_MOUTHS_RATIO 3
 using namespace std;
 
 class Animal {
 public:
-    virtual string makeSound() = 0;
     virtual int eyesNumber() = 0;
-    //virtual Animal& mutated(int mutationRate) = 0;
+    virtual int mouthsNumber() = 0;
+    virtual string sound() = 0;
+    virtual Animal* mutated(int mutationRate) = 0;
 };
 
-class Dog: public Animal{
+class Dog : public Animal {
 private:
+    int numberOfMouths;
     int numberOfEyes;
 public:
-    Dog() {
-        this->numberOfEyes = PREFERABLE_DOGS_EYES_NUMBER;
+    Dog() : Dog(0) {}
+    Dog(int mutationLevel) {
+        this->numberOfMouths = MUTATION_TO_MOUTHS_RATIO * mutationLevel + 1;
+        this->numberOfEyes = PREFERABLE_DOGS_EYES_NUMBER * (mutationLevel + 1);
     }
-    Dog(int numberOfEyes) {
-        this->numberOfEyes = numberOfEyes;
-    }
-    string makeSound(){
-        return "szczek szczek";
+
+    string sound() {
+        string statement = "";
+        for (int i = 0; i < this->numberOfMouths; i++) {
+            statement += "woof woof\n";
+        }
+        return statement;
     }
     int eyesNumber() {
         return this->numberOfEyes;
     }
-    /*Animal& mutated() {
-        //nimal* mutatedVersion = Dog(this->numberOfEyes*2)
-        //A mutatedVersion(this->numberOfEyes*2);
-        return mutatedVersion;
-    }*/
+    int mouthsNumber() {
+        return this->numberOfMouths;
+    }
+    Animal* mutated(int mutationRate) {
+        return new Dog(this->numberOfEyes * mutationRate);
+    }
 };
 
-class Spider: public Animal{
+class Spider : public Animal {
 private:
+    int numberOfMouths;
     int numberOfEyes;
 public:
-    Spider() {
-        this->numberOfEyes = PREFERABLE_SPIDERS_EYES_NUMBER;
+    Spider() : Spider(0) {}
+    Spider(int mutationLevel) {
+        this->numberOfMouths = MUTATION_TO_MOUTHS_RATIO * (mutationLevel) + 1;
+        this->numberOfEyes = PREFERABLE_SPIDERS_EYES_NUMBER * (mutationLevel + 1);
     }
-    Spider(int numberOfEyes) {
-        this->numberOfEyes = numberOfEyes;
-    }
-    string makeSound(){
-        return "...";
+    string sound() {
+        string statement = "";
+        for (int i = 0; i < this->numberOfMouths; i++) {
+            statement += "Making web ... \n";
+        }
+        return statement;
     }
     int eyesNumber() {
         return this->numberOfEyes;
     }
-    /*Animal& mutated() {
-        Spider mutatedVersion(this->numberOfEyes*10);
-        return mutatedVersion;
-    }*/
+    int mouthsNumber() {
+        return this->numberOfMouths;
+    }
+    Animal* mutated(int newMutationLevel) {
+        return new Spider(newMutationLevel);
+    }
 };
 
-int main(int argc, char argv[])
+int main()
 {
-    Dog dog1;
-    Spider spider1;
-    Animal& first = dog1;
-    Animal& second = spider1;
-    //Spider spider2 = spider1.mutated();
-    cout<<first.makeSound()<<endl;
-    cout<<second.eyesNumber()<<endl;
-    //cout<<spider2.eyesNumber();
+
+    Dog perfectlyNormalDog;
+    Spider perfectlyNormalSpider;
+
+    Animal* suspiciousLookingSpider = perfectlyNormalSpider.mutated(2);
+    Animal* suspiciousLookingDog = perfectlyNormalDog.mutated(3);
+    
+    cout << perfectlyNormalDog.sound() << endl;
+    cout << perfectlyNormalSpider.sound() << endl;
+
+    cout << suspiciousLookingDog->eyesNumber() << endl;
+    cout << suspiciousLookingDog->mouthsNumber() << endl;
+    cout << suspiciousLookingDog->sound() << endl;
+
+    cout << suspiciousLookingSpider->eyesNumber() << endl;
+    cout << suspiciousLookingSpider->mouthsNumber() << endl;
+    cout << suspiciousLookingSpider->sound() << endl;
+
     return 0;
 }
